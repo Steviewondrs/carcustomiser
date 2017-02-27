48da6a2801ab530f74e016b60bf2fde3e05754f7
vervaets <steven.vervaet@groups.be>
2017-02-27 13:30:36 +0100

    refactored service-worker to ES7

--- src/service-worker/service-worker.js
+++ src/service-worker/service-worker.js
@@ -15,108 +15,63 @@ const filesToCache = [
     './dist/build.js'
 ];
 
+// // install service worker and add files to cache
 self.oninstall = function( e ) {
     e.waitUntil( ( async () => {
         let cache = await caches.open( CACHE_NAME );
         
         console.log( '[ serviceWorker ]: Cache app shell' );
         await cache.addAll( filesToCache);
+
         console.log( '[ serviceWorker ]: Installed And Skiping Waiting on install');
         self.skipWaiting();
     })() );
 }
 
+// Cache management
+self.onactivate = function( e ) {
+    console.log( '[ serviceWorker ]: Activated' );
+    const whiteList = [ 'car-customiser' ];
+    e.waitUntil( ( async () => {
+        const cacheNames = await caches.keys();
+        for( let key in cacheNames ) {
+            if( whiteList.indexOf( key ) === -1 ) {
+                console.log( '[serviceWorker]: removing old cache', cache );
+                await caches.delete( cache );
+            } 
+        }
 
+        /*
+        This fixes an edge case in which the app wasn't returning the latest data.
+        if you do:
+            1. load app for the first time
+            2. refresh
+            3. go offline
+            4. reload app.
+        then you'd expect to see fresh data, but instead you'll be greeted with wathever
+        initial data you set.
+        */
+        console.log( '[ serviceWorker ]: Clients Claims' );
+        await self.clients.claim();
+    })() );
+}
 
-// // install service worker and add files to cache
-// self.oninstall = function( e ) {
-//     console.log( '[ serviceWorker ]: installing...' );
-//     e.waitUntil(
-//         caches.open( CACHE_NAME )
-//             .then( function( cache )  {
-//                 console.log( '[ serviceWorker ]: Cache app shell' )
-//                 return cache.addAll( filesToCache );
-//             })
-//             .then( function() {
-//                 console.log( '[ serviceWorker ]: Installed And Skiping Waiting on install');
-//                 return self.skipWaiting();
-//             })
-//     )
-// }
+self.onfetch = function( e ) {
+    console.log( '[ serviceWorker ]: Fetch', e.request.url );
+    const DATA_URL = 'https://car-api.firebaseio.com/rest.json';
 
-
-// // Cache management
-// self.onactivate = function( e ) {
-//     console.log( '[ serviceWorker ]: Activated' );
-
-//     var whiteList = [ 'car-customiser' ];
-
-//     e.waitUntil(
-//         caches.keys()
-//             .then( function( cacheList ) {
-//                 return Promise.all( cacheList.map( function( cache ) {
-//                     console.log( '[serviceWorker]: removing old cache', cache );
-//                     return caches.delete( cache );
-//                 }) );
-//             })
-//             .then( function() {
-//                 /*
-//                 This fixes an edge case in which the app wasn't returning the latest data.
-//                 if you do:
-//                     1. load app for the first time
-//                     2. refresh
-//                     3. go offline
-//                     4. reload app.
-//                 then you'd expect to see fresh data, but instead you'll be greeted with wathever
-//                 initial data you set.
-//                 */
-//                 console.log( '[ serviceWorker ]: Clients Claims' );
-//                 return self.clients.claim();
-//             })
-//     )
-// };
-
-// self.onfetch = function( e ) {
-//     console.log( '[ serviceWorker ]: Fetch', e.request.url );
-//     var dataURL = 'https://car-api.firebaseio.com/rest.json';
-
-//     if( e.request.url.indexOf( dataURL ) > -1 ) {
-//         e.respondWith(
-//             caches.open( CACHE_NAME ).then( function( cache ) {
-//                 return fetch( e.request ).then( function( res ){
-//                     cache.put( e.request.url, res.clone() );
-//                     return res;
-//                 });
-//             })
-//         );
-//     } else {
-//         e.respondWith(
-//             caches.match( e.request ).then( function( res ) {
-//                 return res || fetch( e.request );
-//             })
-//         );
-//     }
-// };
-
-
-//     // e.respondWith( 
-//     //     caches.match( e.request ).catch( function() {
-//     //         return fetch( e.request );
-//     //     })
-//     //     .then( function( res ) {
-//     //         caches.open( CACHE_NAME ).then( function( cache ) {
-//     //             cache.put( e.request, res.clone() );
-//     //         })
-//     //         return res;
-//     //     })
-//     //     .catch( function( err ) {
-//     //         console.log( '[ serviceWorker ]: Fetch Error', err );
-//     //     })
-//     // );
-
-// // self.onfetch =function( e ) {
-// //     e.respondWith(
-// //         caches.match( e.request )
-// //             .then( res => res || fetch( e.request ))
-// //     );
-// // }
+    if( e.request.url.indexOf( dataURL ) > -1 ) {
+        e.respondWith( ( async () => {
+            let cache = await caches.open( CACHE_NAME );
+            let response = await fetch( e.request );
+            cache.put( e.request.url, response.clone() );
+            return response;
+        })() );
+    } else {
+        e.respondWith(
+            caches.match( e.request ).then( function( response ) {
+                return response || fetch( e.request );
+            })
+        );
+    }
+};
