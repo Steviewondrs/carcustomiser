7fb61412572e1721b73bfa10a1b3ff32aff8fc6d
Steven Vervaet <stevenvervaet@Stevens-MBP.lan>
2017-02-28 20:42:56 +0100

    SW revisited ( yet again )

--- src/service-worker/service-worker.js
+++ src/service-worker/service-worker.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = 'car-customiser-V1';
+const CACHE_NAME = 'car-customiser-V2';
 // Files we want to cache
 const filesToCache = [
     './',
     './index.html',
     './manifest.json',
     // './serviceworker-cache-polyfill.js',
-    // './src/assets/img/blank-thumbnail.png',
     './src/assets/img/favicon.ico',
     './src/assets/img/icon48.png',
     './src/assets/img/icon72.png',
@@ -17,11 +16,12 @@ const filesToCache = [
 
 // // install service worker and add files to cache
 self.oninstall = function( e ) {
+    console.log( '[ serviceWorker ]: Installing...' );
     e.waitUntil( ( async () => {
         let cache = await caches.open( CACHE_NAME );
-        
-        console.log( '[ serviceWorker ]: Cache app shell' );
-        await cache.addAll( filesToCache);
+
+        console.log( '[ serviceWorker ]: Cache All' );
+        await cache.addAll( filesToCache );
 
         console.log( '[ serviceWorker ]: Installed And Skiping Waiting on install');
         self.skipWaiting();
@@ -31,28 +31,16 @@ self.oninstall = function( e ) {
 // Cache management
 self.onactivate = function( e ) {
     console.log( '[ serviceWorker ]: Activated' );
-    const whiteList = [ 'car-customiser' ];
     e.waitUntil( ( async () => {
         const cacheNames = await caches.keys();
         for( let key in cacheNames ) {
-            if( whiteList.indexOf( key ) === -1 ) {
-                console.log( '[serviceWorker]: removing old cache', key );
+            if( key.indexOf( CACHE_NAME ) === -1 ) {
+                console.log( '[ serviceWorker ]: removing old cache', key );
                 await caches.delete( key );
-            } 
+            }
         }
-
-        /*
-        This fixes an edge case in which the app wasn't returning the latest data.
-        if you do:
-            1. load app for the first time
-            2. refresh
-            3. go offline
-            4. reload app.
-        then you'd expect to see fresh data, but instead you'll be greeted with wathever
-        initial data you set.
-        */
         console.log( '[ serviceWorker ]: Clients Claims' );
-        await self.clients.claim();
+        return self.clients.claim();
     })() );
 }
 
